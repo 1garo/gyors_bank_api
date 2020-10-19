2f2088e51f48186f33736fbf49dcf315f5af853b
1garo <alevardai@hotmail.com>
2020-10-19 18:13:04 -0300

    feat: add poison dependencies

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule BankApi.MixProject do
   def application do
     [
       mod: {BankApi.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :httpoison]
     ]
   end
 
@@ -42,7 +42,9 @@ defmodule BankApi.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:httpoison, "~> 1.7"},
+      {:poison, "~> 4.0"}
     ]
   end
 
