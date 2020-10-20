c87dbd0611bc4d8acc0c3b9917c7c4341cf09de8
1garo <alevardai@hotmail.com>
2020-10-20 16:05:42 -0300

    feat: fetch data from real api

--- lib/bank_api_web/views/user_view.ex
+++ lib/bank_api_web/views/user_view.ex
@@ -6,9 +6,16 @@ defmodule BankApiWeb.UserView do
   end
 
   def render("user.json", %{data: steam_id}) do
-    # resp = HTTPoison.get! "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=C7E685D4E2DD7860F1EADB04C393907A&steamids=#{steam_id}"
-    # Poison.decode(resp.body)
-    IO.inspect steam_id["test"] 
+    resp = HTTPoison.get! "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=C7E685D4E2DD7860F1EADB04C393907A&steamids=#{steam_id["test"]}"
+    resp = Poison.decode(resp.body)
+    ret = elem(resp, 1)
+    # ret
+    %{user_info: ret["response"]}
+    # resp = HTTPoison.get! "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=C7E685D4E2DD7860F1EADB04C393907A&steamids=#{steam_id["test"]}".body
+    # resp.body
+    # |> Poison.decode()
+    # |> elem(1)
+    # IO.inspect steam_id["test"] 
     # %{info: param}
   end
 end
